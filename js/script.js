08a430c2bd6dcd4d2e97c847e6b7be436bc1b467
Vinicius Bastos <viniciusbastos57@gmail.com>
2023-05-07 03:28:13 -0300

    new file:   assets/components/button.css new file:   css/reset.css modified:   css/style.css new file:   img/github.png new file:   img/icone-info.svg new file:   img/linkedin.png new file:   img/logo.svg new file:   img/nao-encontrado.svg modified:   index.html modified:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,67 @@
+document.querySelector(".resposta").style.display = "none"
+
+var inputArea = document.querySelector(".input-text");
+var outputArea = document.querySelector(".output-text");
+
+var btnCripto = document.querySelector(".btn-criptografar");
+var btnDescripto = document.querySelector(".btn-descriptografar");
+var btnCopiar = document.querySelector(".btn-copiar");
+
+var resultado = "";
+
+
+inputArea.addEventListener("input", function () {
+    var textoValidado = inputArea.value.toLowerCase().replace(/[^a-z 0-9\s]/g, "");
+    inputArea.value = textoValidado
+});
+
+function criptografar() {
+
+    if (inputArea.value != "") {
+        document.querySelector(".resposta").style.display = "block"
+        document.querySelector(".mensagem-nao-encontrada").style.display = "none"
+
+
+        resultado = inputArea.value;
+
+        resultado = resultado
+            .replace(/a/g, "ai")
+            .replace(/e/g, "enter")
+            .replace(/i/g, "imes")
+            .replace(/o/g, "ober")
+            .replace(/u/g, "ufat");
+
+        outputArea.innerHTML = resultado;
+
+        inputArea.value = "";
+    } else {
+        alert("Digite a mensagem que quer criptografar!")
+    }
+};
+
+function descriptografar() {
+    if (inputArea.value != "") {
+        document.querySelector(".resposta").style.display = "block"
+        document.querySelector(".mensagem-nao-encontrada").style.display = "none"
+
+
+        resultado = inputArea.value;
+
+        resultado = resultado
+            .replace(/ai/g, "a")
+            .replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/ober/g, "o")
+            .replace(/ufat/g, "u");
+
+        outputArea.innerHTML = resultado;
+
+        inputArea.value = "";
+    } else {
+        alert("Digite a mensagem que quer criptografar!")
+    }
+};
+
+function copiar() {
+    navigator.clipboard.writeText(resultado);
+}
